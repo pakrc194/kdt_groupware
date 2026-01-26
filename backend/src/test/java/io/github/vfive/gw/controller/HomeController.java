09c0d33ab96964b0eafb9810a90b4ada7e21e63f
pakrc <pakrc194@gmail.com>
2026-01-26 21:50:16 +0900

    backend init 0126

---
+++ backend/src/test/java/io/github/vfive/gw/controller/HomeController.java
@@ -0,0 +1,14 @@
+package io.github.vfive.gw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@RequestMapping
+	String home() {
+		return "home";
+	}
+}
