90abbbf41cd66da8d4e302e3b7e39fabc9059c6c
sett <kguswls2684@naver.com>
2026-02-10 18:24:54 +0900

    26_02_10 fetch

--- frontend/src/features/Home/pages/HomeModProf.js
+++ frontend/src/features/Home/pages/HomeModProf.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import "../css/HomeModProf.css";
+import styles from "../css/HomeModProf.module.css";
 import { fetcher } from "../../../shared/api/fetcher";
 import dayjs from "dayjs";
 
